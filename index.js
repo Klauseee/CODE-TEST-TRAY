8b29c7f8582dddf0d2cd39d3beb01310474fd11e
Nic Wilson <nicholaswilson3010@gmail.com>
2018-07-10 10:22:45 +0100

    Dirt checked and removed, cleaned tally calculated and final hoover position logged, need to test and output correctly

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const rows = input.split('\n'); // split on each line break
 const dimensions = rows[0];
 const hooverPosition = { x: rows[1][0], y: rows[1][2] };
 const instructions = rows[rows.length - 2].split(''); // array keeps taking white space from end of txt file
+// clone rows so that rows itself isn't altered with splice
+const rowsCopy = rows.slice();
+const dirt = rowsCopy.splice(2, (rows.length - 4));
+let cleaned = 0;
+
 
 const movements = {
   N: () => {
@@ -22,7 +27,19 @@ const movements = {
   }
 };
 
+// check whether hoover has gone over a dirt patch, add to cleaned tally and make sure it can't clean the same dirt patch more than once
+function checkDirt() {
+  if(dirt.includes(`${hooverPosition.x} ${hooverPosition.y}`)) {
+    cleaned ++;
+    const index = dirt.indexOf(`${hooverPosition.x} ${hooverPosition.y}`);
+    dirt.splice(index, 1);
+  }
+}
+
 // run through each instruction and make the relevant movement
 instructions.forEach(instruction => {
   movements[instruction]();
+  checkDirt();
 });
+
+console.log(hooverPosition, cleaned);
